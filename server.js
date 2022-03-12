480f50cde72d4017d4cd34b475dc2da0debea7f1
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-12 11:00:11 -0700

    adding notes route

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+const {notes} = require('./db/db.json');
 
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+    
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+        
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query);
+    res.json(results);
 });
 
 app.listen(3001, () => {
