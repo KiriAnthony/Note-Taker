01da17635e56f37f42db1e4f7768492851eaed16
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-17 16:34:04 -0700

    updating routes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const apiRoutes = require('./apiRoutes');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 
-const {notes} = require('./db/db.json');
 
 // const fs = require('fs');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
 
 // display index.html data
 app.get('/', (req, res) => {
@@ -18,12 +22,6 @@ app.get('/', (req, res) => {
 //display notes.html data
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-
-    res.json(results);
 });
 
 app.listen(PORT, () => {
